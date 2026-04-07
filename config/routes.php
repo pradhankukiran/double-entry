843026107e4ad00cffcfb6564c07cf1df45fe7e5
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-04-07 19:49:10 +0530

    Add routes for contacts, invoices, bills, and payments

--- config/routes.php
+++ config/routes.php
@@ -52,4 +52,27 @@ return [
     ['method' => 'GET', 'path' => '/reports/income-statement', 'controller' => 'ReportController', 'action' => 'incomeStatement', 'middleware' => ['AuthMiddleware']],
     ['method' => 'GET', 'path' => '/reports/cash-flow', 'controller' => 'ReportController', 'action' => 'cashFlow', 'middleware' => ['AuthMiddleware']],
     ['method' => 'GET', 'path' => '/reports/{report}/pdf', 'controller' => 'ReportController', 'action' => 'exportPdf', 'middleware' => ['AuthMiddleware']],
+
+    // Contacts
+    ['method' => 'GET', 'path' => '/contacts', 'controller' => 'ContactController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/contacts/create', 'controller' => 'ContactController', 'action' => 'create', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/contacts', 'controller' => 'ContactController', 'action' => 'store', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/contacts/{id}', 'controller' => 'ContactController', 'action' => 'show', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/contacts/{id}/edit', 'controller' => 'ContactController', 'action' => 'edit', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/contacts/{id}', 'controller' => 'ContactController', 'action' => 'update', 'middleware' => ['AuthMiddleware']],
+
+    // Invoices & Bills
+    ['method' => 'GET', 'path' => '/invoices', 'controller' => 'InvoiceController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/invoices/create', 'controller' => 'InvoiceController', 'action' => 'create', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/invoices', 'controller' => 'InvoiceController', 'action' => 'store', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/invoices/{id}', 'controller' => 'InvoiceController', 'action' => 'show', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/invoices/{id}/post', 'controller' => 'InvoiceController', 'action' => 'post', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/invoices/{id}/void', 'controller' => 'InvoiceController', 'action' => 'void', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/bills', 'controller' => 'InvoiceController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+
+    // Payments
+    ['method' => 'GET', 'path' => '/payments', 'controller' => 'PaymentController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/payments/create', 'controller' => 'PaymentController', 'action' => 'create', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/payments', 'controller' => 'PaymentController', 'action' => 'store', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/payments/{id}', 'controller' => 'PaymentController', 'action' => 'show', 'middleware' => ['AuthMiddleware']],
 ];
