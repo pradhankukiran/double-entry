3a48e1873c4d1e8fe0815f80d5c2ac5f5dbbcf37
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-04-07 19:26:36 +0530

    Add routes for chart of accounts and fiscal years

--- config/routes.php
+++ config/routes.php
@@ -18,4 +18,18 @@ return [
     ['method' => 'GET', 'path' => '/users', 'controller' => 'UserController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
     ['method' => 'GET', 'path' => '/profile', 'controller' => 'UserController', 'action' => 'profile', 'middleware' => ['AuthMiddleware']],
     ['method' => 'POST', 'path' => '/profile', 'controller' => 'UserController', 'action' => 'updateProfile', 'middleware' => ['AuthMiddleware']],
+
+    // Chart of Accounts
+    ['method' => 'GET', 'path' => '/accounts', 'controller' => 'AccountController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/accounts/create', 'controller' => 'AccountController', 'action' => 'create', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/accounts', 'controller' => 'AccountController', 'action' => 'store', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/accounts/{id}', 'controller' => 'AccountController', 'action' => 'show', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/accounts/{id}/edit', 'controller' => 'AccountController', 'action' => 'edit', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/accounts/{id}', 'controller' => 'AccountController', 'action' => 'update', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/accounts/{id}/toggle', 'controller' => 'AccountController', 'action' => 'toggleActive', 'middleware' => ['AuthMiddleware']],
+
+    // Fiscal Years
+    ['method' => 'GET', 'path' => '/fiscal-years', 'controller' => 'FiscalYearController', 'action' => 'index', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'GET', 'path' => '/fiscal-years/create', 'controller' => 'FiscalYearController', 'action' => 'create', 'middleware' => ['AuthMiddleware']],
+    ['method' => 'POST', 'path' => '/fiscal-years', 'controller' => 'FiscalYearController', 'action' => 'store', 'middleware' => ['AuthMiddleware']],
 ];
